ee564fc8e438d1e84b9eab084b586ea060e55402
Jardel Schaefer <jardel@demacode.com.br>
2020-06-25 08:20:53 -0300

    Começando a mexer com criteria

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,12 +1,19 @@
 package com.algaworks.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
@@ -20,13 +27,31 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	@Override
 	public List<Restaurante> find (String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
 		
-		var jpql = "from Restaurante where nome like :nome "
-				+ "and taxaFrete between :taxaInicial and :taxaFinal";
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		
-		return manager.createQuery(jpql, Restaurante.class)
-				.setParameter("nome", "%"+ nome +"%")
-				.setParameter("taxaInicial", taxaFreteInicial)
-				.setParameter("taxaFinal", taxaFreteFinal)
-				.getResultList();
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		Root<Restaurante> root = criteria.from(Restaurante.class);
+		
+		var predicates = new ArrayList<Predicate>();
+		
+		if (StringUtils.hasLength(nome)) {
+			predicates.add(builder
+					.like(root.get("nome"), "%"+ nome +"%"));
+		}
+
+		if(taxaFreteInicial != null) {
+			predicates.add(builder
+					.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+		}
+		
+		if(taxaFreteFinal != null) {
+			predicates.add(builder
+					.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+		}
+		
+		criteria.where(predicates.toArray(new Predicate[0]));		
+		
+		return manager.createQuery(criteria).getResultList();
+
 	}
 }
